85b099a9be60cbae74494c43ec2fa68d51a2ea4c
russellkoons <rkoons@gmail.com>
2020-09-24 06:20:55 -0400

    timer

--- scripts.js
+++ scripts.js
@@ -1,5 +1,24 @@
+let countdown;
+
 function timer(secs) {
   const now = Date.now();
   const then = now + secs * 1000;
-  console.log(now, then);
+  displayTimeLeft(secs);
+  
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) * 1000);
+
+    // check if we should stop
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+
+    // display countdown
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(secs) {
+  console.log(secs);
 }
