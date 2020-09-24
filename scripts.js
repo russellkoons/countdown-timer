b5bef945d97479c7f88bc178856c29d06e069f8b
russellkoons <rkoons@gmail.com>
2020-09-24 06:37:04 -0400

    startTimer

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(secs) {
   const now = Date.now();
@@ -37,3 +38,10 @@ function displayEndTime(timestamp) {
   const mins = end.getMinutes();
   endTime.textContent = `Be back at ${hour > 12 ? hour - 12 : hour}:${mins < 10 ? '0' : ''}${mins}`;
 }
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
