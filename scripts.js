4966d9cefa26108e1c9d84a47c90a6792be46557
russellkoons <rkoons@gmail.com>
2020-09-24 06:34:46 -0400

    displayendtime

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(secs) {
   const now = Date.now();
   const then = now + secs * 1000;
   displayTimeLeft(secs);
+  displayEndTime(then);
   
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -24,6 +26,14 @@ function displayTimeLeft(secs) {
   const mins = Math.floor(secs / 60);
   const remainder = secs % 60;
 
-  const display = `${mins}:${remainder}`;
+  const display = `${mins}:${remainder < 10 ? '0' : ''}${remainder}`;
   timerDisplay.textContent = display;
+  document.title = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const mins = end.getMinutes();
+  endTime.textContent = `Be back at ${hour > 12 ? hour - 12 : hour}:${mins < 10 ? '0' : ''}${mins}`;
 }
